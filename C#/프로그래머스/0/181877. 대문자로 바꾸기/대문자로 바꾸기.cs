754953dfb42526c0242917bf64e5058f15d44980
PAKJUNGMIN <138805809+TheMoneyIsAlwaysRight@users.noreply.github.com>
2024-09-01 19:36:57 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 0.56 ms, Memory: 31 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution {
+    public string solution(string myString) {
+        string answer = myString.ToUpper();;
+        
+        
+        return answer;
+    }
+}

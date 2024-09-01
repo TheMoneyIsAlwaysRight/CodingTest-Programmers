96864aa8cc9ef4f3ccf9ab9a606b72134e5b8517
PAKJUNGMIN <138805809+TheMoneyIsAlwaysRight@users.noreply.github.com>
2024-09-01 19:25:12 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.32 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[n];
+        
+        for(int x=0;x<n;x++)
+        {
+            answer[x] = num_list[x];
+        }
+        
+        
+        
+        return answer;
+    }
+}

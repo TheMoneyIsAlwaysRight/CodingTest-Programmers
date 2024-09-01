14b9656d5491f9c9a64fe1e9cdf83de4b01a415b
PAKJUNGMIN <138805809+TheMoneyIsAlwaysRight@users.noreply.github.com>
2024-09-01 19:46:15 +0900

    [level 1] Title: 평균 구하기, Time: 0.81 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ C#/프로그래머스/1/12944. 평균 구하기/평균 구하기.cs
@@ -0,0 +1,10 @@
+using System.Linq;
+using System;
+
+public class Solution {
+    public double solution(int[] arr) {
+        double answer = arr.Average();
+        
+        return answer;
+    }
+}

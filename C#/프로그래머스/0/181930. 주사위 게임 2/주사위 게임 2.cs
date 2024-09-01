72d62f3ac01db11639ef3a6e6c71658af6aa8636
PAKJUNGMIN <138805809+TheMoneyIsAlwaysRight@users.noreply.github.com>
2024-09-01 19:32:29 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.42 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Solution {
+    public int solution(int a, int b, int c)
+    {
+        double result;
+        if ((a != b) && (b != c) && (a != c))
+        {
+            result = a + b + c;
+        }
+        else if ((a == b) && (b == c))
+        {
+            result = (a + b + c) * (Math.Pow(a, 2) + Math.Pow(b, 2) + Math.Pow(c, 2)) * (Math.Pow(a, 3) + Math.Pow(b, 3) +              Math.Pow(c, 3));
+        }
+        else
+        {
+
+            result = (a + b + c) * (Math.Pow(a, 2) + Math.Pow(b, 2) + Math.Pow(c, 2));
+        }
+
+        return (int)result;
+
+    }
+}

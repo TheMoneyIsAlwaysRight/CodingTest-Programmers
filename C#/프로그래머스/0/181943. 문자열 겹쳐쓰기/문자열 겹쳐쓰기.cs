047ab0530ff5985e25dc0a86fd894c183ad21388
PAKJUNGMIN <138805809+TheMoneyIsAlwaysRight@users.noreply.github.com>
2024-09-01 19:35:23 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 2.24 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution {
+    public string solution(string my_string, string overwrite_string, int s)
+    {
+        string answer = "";
+
+        List<char> prev_List = my_string.ToCharArray().ToList<Char>();
+        List<char> new_List = overwrite_string.ToCharArray().ToList<Char>();
+
+        int x = 0;
+        while (prev_List.Count > 0)
+        {
+            if (x < s)
+            {
+                answer += prev_List[0];
+                prev_List.RemoveAt(0);
+            }
+            else if (x >= s)
+            {
+                if (new_List.Count > 0)
+                {
+                    answer += new_List[0];
+                    new_List.RemoveAt(0);
+                    prev_List.RemoveAt(0);
+                }
+                else
+                {
+                    answer += prev_List[0];
+                    prev_List.RemoveAt(0);
+                }
+            }
+            x++;
+        }
+        return answer;
+
+    }
+}

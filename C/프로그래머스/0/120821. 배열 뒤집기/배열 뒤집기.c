9631ad81d77786f4b1893071c958708c0eb27e60
PAKJUNGMIN <138805809+TheMoneyIsAlwaysRight@users.noreply.github.com>
2025-03-18 11:28:38 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.01 ms, Memory: 4.54 MB -BaekjoonHub

--- C/프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.c
+++ C/프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.c
@@ -6,7 +6,7 @@
 int* solution(int num_list[], size_t num_list_len)
 {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
-    int* answer = (int*)malloc(num_list_len);
+    int* answer = (int*)malloc(num_list_len * sizeof(int));
     
     int count = 0;
     

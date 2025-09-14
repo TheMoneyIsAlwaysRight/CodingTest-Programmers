82d1d7ecc2a635fb030fd88e73de14c7a92a502e
PAKJUNGMIN <138805809+TheMoneyIsAlwaysRight@users.noreply.github.com>
2025-09-15 00:27:00 +0900

    [level 0] Title: 수 조작하기 2, Time: 0.77 ms, Memory: 7.04 MB -BaekjoonHub

--- C/프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.c
+++ C/프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.c
@@ -2,15 +2,16 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+// numLog_len은 배열 numLog의 길이입니다.
 char* solution(int numLog[], size_t numLog_len)
 {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
-    char* answer = (char*)malloc((numLog_len) * sizeof(char));
+    char* answer = (char*)malloc((numLog_len+1) * sizeof(char));
+    //NULL 문자 포함한 길이 동적 할당.
 
-    for (int x = 0; x <= numLog_len; x++)
+    for (int x = 0; x < numLog_len; x++)
     {
         if (x == 0) { continue; }
-
         int abs = numLog[x] - numLog[x - 1];
 
         switch (abs)
@@ -19,18 +20,20 @@ char* solution(int numLog[], size_t numLog_len)
             answer[x-1] = 'w';
             break;
         case -1:
-            answer[x-1] = 's';
+            answer[x - 1] = 's';
             break;
         case 10:
-            answer[x-1] = 'd';
+            answer[x - 1] = 'd';
             break;
         case -10:
-            answer[x-1] = 'a';
+            answer[x - 1] = 'a';
             break;
         default:
-            continue;
+            break;
         }
     }
     
+    answer[numLog_len-1] = '\0';
+
     return answer;
 }
